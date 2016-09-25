58454e7f53ea655db849036623a0594e0fd2ec97
manoj.ampolu <full.of.manoj@gmail.com>
2016-09-25 15:00:52 +0530

    changed post method

--- kwyjibo/get_ad_data.py
+++ kwyjibo/get_ad_data.py
@@ -7,8 +7,19 @@ import json
 serverUrl = 'http://ec2-54-169-111-88.ap-southeast-1.compute.amazonaws.com/api/receiveAd' 
 custId = 1234
 def get_latest_ads():
+    resp = {}
     try:
-        resp = requests.post(serverUrl, data=json.dumps({'custId': custId}))
+        resp = requests.get(serverUrl, data=json.dumps({'custId': custId, 'city':'Chennai', 'region':'adyar'}))
         resp = json.loads(resp)
     except:
         print traceback.format_exc()
+    return resp
+
+def change_key_names():
+    resp = get_latest_ads()
+    resp = resp.get('Response')
+    if not resp:
+        return {}
+    print resp
+
+change_key_names()
