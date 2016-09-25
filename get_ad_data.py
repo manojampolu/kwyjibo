aa5bb66f24a60d9afbdde85ff36de63035709994
manoj.ampolu <full.of.manoj@gmail.com>
2016-09-25 15:54:31 +0530

    made changes to html fils

--- get_ad_data.py
+++ get_ad_data.py
@@ -22,5 +22,27 @@ def change_key_names():
     resp = resp.get('Response')
     if not resp:
         return {}
+    retDict = {'t1':"Live Sports Feed", 
+                't1l1':resp.get("Live Sports Feed", {})[0][0], 
+                't1l2':resp.get("Live Sports Feed", {})[0][1],
+                't1l3':resp.get("Live Sports Feed", {})[0][2],
+                't1l4':resp.get("Live Sports Feed", {})[0][3],
+                't2':"Top News", 
+                't2l1':resp.get('news', {}).get("Top News", {})[0],
+                't2l2':resp.get('news', {}).get("Top News", {})[1],
+                't2l3':resp.get('news', {}).get("Top News", {})[2],
+                't2l4':resp.get('news', {}).get("Top News", {})[3],
+                't3':"SENSEX", 
+                't3l1':'BSE '+str(resp.get("stockMarket", {}).get("SENSEX", {}).get("current", 28000)),
+                't3l2':'NSE '+str(resp.get("stockMarket", {}).get("NIFTY", {}).get("current", 8500)),
+                't4':"Commodities", 
+                't4l1':"Gold(10gm) "+str(resp.get("commodities", {}).get("gold", 30000)),
+                't4l2':"Silver(1kg) "+str(resp.get("commodities", {}).get("silver", 47000)),
+                't4l3':"1$ = Rs."+str(resp.get("commodities", {}).get("usdInr", 65)),
+                'w1': resp.get("weather", {}).get("temp", 30),
+                }
+    tmpDict = dict([('img'+str(inx),val) for inx, val in enumerate(resp.get('ads'))])
+    retDict.update(tmpDict)
+    return retDict
 
-change_key_names()
+print change_key_names()
